509dcfa8c2e01701e3ce74a5b198329b1e4420cd
James Montemagno <james.montemagno@gmail.com>
2021-09-16 13:42:22 -0700

    Add project files.

---
+++ OfficeControl/OfficeControl/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OfficeControl
+{
+    public class Constants
+    {
+        public const string APIKey = ""; 
+    }
+}
---
+++ OfficeControl/OfficeControl/MainPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OfficeControl
+{
+    public partial class MainPage : ContentPage
+    {
+        string turnOnUrl = $"https://maker.ifttt.com/trigger/turn_on_office_light/with/key/{Constants.APIKey}";
+        string turnOffUrl = $"https://maker.ifttt.com/trigger/turn_off_office_light/with/key/{Constants.APIKey}";
+        string changeColorUrl = $"https://maker.ifttt.com/trigger/change_office_color/with/key/{Constants.APIKey}";
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
